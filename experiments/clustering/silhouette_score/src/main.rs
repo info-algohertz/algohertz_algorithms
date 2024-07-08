fc97bdb88cbad3ae71f985bcb1f9df6b769863f2
Dávid Natingga <info@algohertz.com>
2024-07-08 14:03:31 +0200

    Implement compute_silhouette_score

--- experiments/clustering/silhouette_score/src/main.rs
+++ experiments/clustering/silhouette_score/src/main.rs
@@ -10,7 +10,6 @@ use std::io::Result;
 use std::fs::File;
 use std::collections::HashMap;
 
-// TODO: compute median instead as it is more robust than the mean in the noisy
 fn compute_mean(values: &Vec<f64>) -> f64 {
     let mut sum: f64 = 0.0;
     for v in values {
@@ -18,15 +17,6 @@ fn compute_mean(values: &Vec<f64>) -> f64 {
     }
     let mean: f64 = sum/values.len() as f64;
     return mean;
-}
-
-fn compute_centroid(dim_count: &usize, points: &Vec<Vec<f64>>) -> Vec<f64> {
-    let mut centroid = vec![];
-    for i in 0..*dim_count {
-        let mean = compute_mean(&points[i]);
-        centroid.push(mean);        
-    }
-    return centroid;
 }
 
 fn euclidean_metric(point_a: &Vec<f64>, point_b: &Vec<f64>) -> f64 {
@@ -44,8 +34,8 @@ fn euclidean_metric(point_a: &Vec<f64>, point_b: &Vec<f64>) -> f64 {
 
 fn compute_mean_distance(points: &Vec<Vec<f64>>, point: &Vec<f64>, dist_metric: fn(&Vec<f64>, &Vec<f64>) -> f64) -> f64 {
     let mut distances: Vec<f64> = vec![];
-    for point in points {
-        let d = dist_metric(&points[0], point);
+    for point2 in points {
+        let d = dist_metric(point, point2);
         distances.push(d);
     }
     return compute_mean(&distances);
@@ -54,10 +44,28 @@ fn compute_mean_distance(points: &Vec<Vec<f64>>, point: &Vec<f64>, dist_metric: 
 fn compute_silhouette_score(dim_count: &usize, clusters: &HashMap<i64, Vec<Vec<f64>>>) -> f64 {
     let mut scores: Vec<f64> = vec![];
     let cluster_numbers = clusters.keys();
-    for cluster_number in cluster_numbers {
+    for cluster_number in cluster_numbers.clone() {
         let points = clusters.get(&cluster_number).unwrap();
-        let d = compute_mean_distance(&points, &points[0], euclidean_metric);
-        scores.push(d);
+        let ratio = points.len() as f64/(points.len() as f64 - 1.0);
+        for point in points {
+            let d = compute_mean_distance(&points, &point, euclidean_metric);
+            let a = ratio*d;
+            
+            //let distances: Vec<f64> = vec![];
+            let mut b = f64::INFINITY;
+            for cluster_number2 in cluster_numbers.clone() {
+                if cluster_number == cluster_number2 {
+                    continue;
+                }
+                let points2 = clusters.get(&cluster_number2).unwrap();
+                let d = compute_mean_distance(&points2, &point, euclidean_metric);
+                if d < b {
+                    b = d;
+                }
+            }
+            let s = (b - a)/a.max(b);
+            scores.push(s);
+        }
     }
     let score = compute_mean(&scores);
     return score;
@@ -104,8 +112,9 @@ fn main() -> Result<()> {
         clusters.get_mut(&cluster).unwrap().push(point);
     }
     
-    let centroid = compute_centroid(&dim_count, clusters.get(&0).unwrap());
-    dbg!(centroid);
+
+    let silhouette_score = compute_silhouette_score(&dim_count, &clusters);
+    println!("{:?}", silhouette_score);
     
     Ok(())
 }
